e08204b28074763c19934a39c48ab2463e016c0d
takajin <takajin@ubuntu.ubuntu-domain>
2018-02-22 22:24:04 +0900

    delete unnecessary import

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import tensorflow as tf
 import tensorflow.python.platform
 from PIL import Image
 import scipy
-from PyQt4 import QtCore, QtGui
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
 from scipy import ndimage
 from image_data_set import ImageDataSet
 # read config.ini
